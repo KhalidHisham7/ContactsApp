924d56fd333f4a87e0e5821f743dcabd615baf9f
Khalid <timoncr9@gmail.com>
2018-05-11 19:09:42 +0200

    Basic components set, forms are created

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Contact;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,37 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group(['middleware' => 'api'], function(){
+  //Fetches contacts
+  Route::get('contacts', function(){
+    return Contact::latest()->orderBy('created_at', 'desc')->get();
+  });
+
+  //Getting single Contacts
+  Route::get('contact/{id}', function($id){
+    return Contact::findOrFail($id);
+  });
+
+  //Adding a contact
+  Route::post('contact/store', function(Request $request){
+    return Contact::create(['name' => $request->input(['name']),
+                            'email' => $request->input(['email']),
+                            'phone' => $request->input(['phone'])]);
+  });
+
+  //Updating a contact
+  Route::patch('contact/{id}', function(Request $request, $id){
+      return Contact::findOrFail($id)->update(['name' => $request->input(['name']),
+                                        'email' => $request->input(['email']),
+                                        'phone' => $request->input(['phone'])]);
+  });
+
+  //Deleting a contact
+  Route::delete('contact/{id}', function($id){
+    return Contact::destroy($id);
+  });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
